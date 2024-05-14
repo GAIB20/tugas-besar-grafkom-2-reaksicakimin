5fa0e26c21d087c115f4c0b53828973f095a3fbd
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-05-14 18:46:28 +0700

    feat: add Vector3 utils class

---
+++ src/script/utils/Vector3.js
@@ -0,0 +1,35 @@
+export class Vector3 {
+    constructor(x = 0, y = 0, z = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    add(v) {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+        return this;
+    }
+
+    subtract(v) {
+        this.x -= v.x;
+        this.y -= v.y;
+        this.z -= v.z;
+        return this;
+    }
+
+    length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    }
+
+    normalize() {
+        const len = this.length();
+        if (len === 0) return this;
+        return this.multiplyScalar(1 / len);
+    }
+
+    clone() {
+        return new Vector3(this.x, this.y, this.z);
+    }
+}
